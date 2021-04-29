c1c1f72dc439431f70b7a558256adb1acf63b551
Gabriel Viana <gabrielv14@gmail.com>
2021-04-28 22:05:24 -0300

    testing twilio api

--- src/main/java/com/labs/luizalabschallenge/LuizalabsChallengeApplication.java
+++ src/main/java/com/labs/luizalabschallenge/LuizalabsChallengeApplication.java
@@ -1,5 +1,7 @@
 package com.labs.luizalabschallenge;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,12 +10,20 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.labs.luizalabschallenge")
-@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
 public class LuizalabsChallengeApplication {
-
+    public static final String ACCOUNT_SID = "AC1c009f66925e669230b8948a4912ab39";
+    public static final String AUTH_TOKEN = "";
     public static void main(String[] args) {
         SpringApplication.run(LuizalabsChallengeApplication.class, args);
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+556196302228"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                "Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10.")
+                .create();
+
+        System.out.println(message.getSid());
     }
 
 }
--- src/main/java/controllers/impl/NoticeControllerImpl.java
+++ src/main/java/controllers/impl/NoticeControllerImpl.java
@@ -2,9 +2,20 @@ package controllers.impl;
 
 import controllers.NoticeController;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(value = "Notice", tags = {"Notice"})
 public class NoticeControllerImpl implements NoticeController {
+//    @Value("${phoneNumber:** Todo: buy a phone number from Twilio **}")
+//    private String phoneNumber;
+
+//    @GetMapping("/")
+//    public ModelAndView showDashboard(){
+//        ModelAndView dashboard = new ModelAndView("dashboard");
+//        dashboard.addObject("phoneNumber", phoneNumber);
+//        return dashboard;
+//    }
 }
